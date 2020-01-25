1ea2bfd01e2ed09ffe95d39ff355108efa1bee81
Brady Bridges <bradyjbridges@gmail.com>
2020-01-25 11:04:27 -0700

    Add connection to firestore, run test fetch to pull all messages and display

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import * as firebase from 'firebase';
+import 'firebase/firestore';
 import ApiKeys from './ApiKeys';
+import Header from './Components/Header/Header';
 
 export default class App extends Component {
-  componentDidMount = () => {
-    if(!firebase.apps.length) firebase.initializeApp(ApiKeys.FirebaseConfig);
+  state = {
+    messages: [],
+  }
+  componentDidMount = async () => {
+    if(!firebase.apps.length) {
+      await firebase.initializeApp(ApiKeys.FirebaseConfig);
+      this.getUsers();
+    } 
+  }
+  getUsers = async () => {
+    const snapshot = await firebase.firestore().collection('messages').get();
+    const messages = snapshot.docs.map((doc) => doc.data());
+    this.setState({ messages });
+  }
+  renderMessages = () => {
+    return this.state.messages.map((message) => {
+      return (
+        <View>
+          <Text>{message.to}</Text>
+          <Text>{message.from}</Text>
+          <Text>{message.contents}</Text>
+          <Text>{message.sent.seconds}</Text>
+        </View>
+      );  
+    });
   }
   render() {
     return (
       <View style={{paddingTop: 20}}>
-        <Text> textInComponent </Text>
+        <Header />
+        {this.state.messages.length !== 0 && this.renderMessages()}
       </View>
     )
   }
 }
+
 
