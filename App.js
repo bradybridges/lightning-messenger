0baa5c316937dbfbc8553ac28fdace89053b12e7
brady <bradyjbridges@gmail.com>
2020-02-16 10:08:59 -0700

    Add logout button in right side of header

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Home from './Screens/Home/Home';
 import Login from './Screens/Login/Login';
 import CreateAccount from './Screens/CreateAccount/CreateAccount';
+import SignoutIconButton from './Components/SignoutIconButton/SignoutIconButton';
+import { Text } from 'react-native';
 
 const MainNavigator = createStackNavigator({
   Login: {screen: Login},
-  Home: {screen: Home},
+  Home: {screen: Home, navigationOptions: {headerRight: () => <SignoutIconButton />}},
   CreateAccount: {screen: CreateAccount},
 });
 
