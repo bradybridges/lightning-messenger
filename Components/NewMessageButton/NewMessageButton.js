facf205012fc3e79fa15e56b5b70286c73284d16
brady <bradyjbridges@gmail.com>
2020-02-16 19:02:16 -0700

    Add basic functionality and styling

---
+++ Components/NewMessageButton/NewMessageButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import * as Constants from '../../Constants/Constants';
+
+const NewMessageButton = ({ handleNewConversation }) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => handleNewConversation('test@test.com')}>
+      <Text style={styles.text}>+</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Constants.secondaryBgColor,
+    width: 50,
+    height: 50,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  text: {
+    fontSize: 52,
+    color: 'white',
+  },
+});
+
+
+export default NewMessageButton
