59e4509452fdf52fad78c31cb8f9a3e8874bbc6f
Brady Bridges <bradyjbridges@gmail.com>
2020-02-18 16:47:47 -0700

    Add basic functionality to take function from parent that closes modal on press

---
+++ Components/BackButton/BackButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet, Dimensions, StatusBar } from 'react-native';
+const backIcon = require('../../assets/back-arrow-white.png');
+
+const BackButton = ({ close }) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={close}>
+      <Image style={styles.image} source={backIcon} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+    width: 40,
+    height: 40,
+    position: 'absolute',
+    zIndex: 10,
+    left: 5,
+    top: Dimensions.get('window').height * .05,
+    marginTop: -20,
+  },
+  image: {
+    width: 40,
+    height: 40,    
+  }
+});
+
+
+export default BackButton;
