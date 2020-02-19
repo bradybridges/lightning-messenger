126c6e527733593ace823dc5b6438ece8260e1ff
Brady Bridges <bradyjbridges@gmail.com>
2020-02-18 19:48:04 -0700

    Change new message button to icon

--- Components/NewMessageButton/NewMessageButton.js
+++ Components/NewMessageButton/NewMessageButton.js
@@ -1,33 +1,27 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import * as Constants from '../../Constants/Constants';
+const addIcon = require('../../assets/add-icon.png');
 
 const NewMessageButton = ({ toggleNewConversation }) => {
   return (
     <TouchableOpacity style={styles.container} onPress={() => toggleNewConversation()}>
-      <Text style={styles.text}>+</Text>
+      <Image source={addIcon} style={styles.image}/>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Constants.secondaryBgColor,
-    width: 50,
-    height: 50,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50,
+    width: 65,
+    height: 65,
     position: 'absolute',
-    bottom: 10,
-    right: 10,
-    borderColor: 'white',
-    borderWidth: 1,
+    bottom: 15,
+    right: 15,
   },
-  text: {
-    fontSize: 52,
-    color: 'white',
+  image: {
+    width: 65, 
+    height: 65,
   },
 });
 
