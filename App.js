bf54e3615d6a44c9ea5aae933873121a10cc9e85
brady <bradyjbridges@gmail.com>
2020-03-07 12:12:24 -0700

    Update stack navigation header settings

--- App.js
+++ App.js
@@ -5,13 +5,48 @@ import Home from './Screens/Home/Home';
 import Login from './Screens/Login/Login';
 import CreateAccount from './Screens/CreateAccount/CreateAccount';
 import SignoutIconButton from './Components/SignoutIconButton/SignoutIconButton';
-import { Text } from 'react-native';
+import { View, Text, Image, BlurView, StyleSheet } from 'react-native';
 
 const MainNavigator = createStackNavigator({
-  Login: {screen: Login},
-  Home: {screen: Home, navigationOptions: {headerRight: () => <SignoutIconButton />}},
-  CreateAccount: {screen: CreateAccount},
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      headerShown: false,
+    }
+  },
+  Home: {
+    screen: Home, 
+    navigationOptions: {
+      headerRight: () => <SignoutIconButton />, 
+      headerTitle: () => renderLogo(),
+      headerTitleAlign: () => 'center',
+      headerStyle: {
+        backgroundColor: 'black',
+      },
+    },
+  },
+  CreateAccount: { screen: CreateAccount },
 });
+
+const renderLogo = () => {
+  return (
+    <View>
+      <Image source={require('./assets/logo.png')} style={{ width: 50, height: 50 }} />
+    </View>
+  );
+}
+
+const title = () => {
+  return (
+    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>LightningMessenger</Text>
+      <Image 
+        source={require('./assets/logo.png')}
+        style={{ width: 100, height: 100 }}       
+      />
+    </View>
+  )
+}
 
 const App = createAppContainer(MainNavigator);
 
