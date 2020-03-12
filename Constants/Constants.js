e6a4bbd118a4352807e01fe9a9f86a333f28b96e
brady <bradyjbridges@gmail.com>
2020-03-12 15:46:02 -0600

    Update default color values for new theme, add margin/padding default vals

--- Constants/Constants.js
+++ Constants/Constants.js
@@ -1,2 +1,7 @@
-export const primaryBgColor = '#000000d6';
+export const primaryBgColor = '#14272E';
 export const secondaryBgColor ='#0000ffc7';
+export const tertiaryBgColor = '#6E8F76';
+export const primaryHeaderColor = '#121716';
+export const baseMarginPadding = 10;
+export const midMarginPadding = 20;
+export const highMarginPadding = 40;
