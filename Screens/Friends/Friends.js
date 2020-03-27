f5d149b8239613c78eaf8a8a1975bbec9b501a48
brady <bradyjbridges@gmail.com>
2020-03-26 22:08:52 -0600

    Add basic friends screen layout and styling

--- Screens/Friends/Friends.js
+++ Screens/Friends/Friends.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import * as Constants from '../../Constants/Constants';
 
 export default class Friends extends Component {
   render() {
     return (
-      <View>
-        <Text> Friends </Text>
+      <View style={styles.container}>
+        <Text style={styles.text}> Friends </Text>
+        <TouchableOpacity style={styles.addFriendContainer}>
+          <Text style={styles.addFriendText}>
+            Add Friend
+          </Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Constants.primaryBgColor,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  text: {
+    color: Constants.tertiaryBgColor,
+    fontSize: 32,
+  },
+  addFriendContainer: {
+    width: 150,
+    backgroundColor: Constants.primaryHeaderColor,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 50,
+    borderColor: Constants.tertiaryBgColor,
+    borderWidth: 1,
+    position: 'absolute',
+    bottom: 20,
+    right: 10,
+  },
+  addFriendText: {
+    color: Constants.tertiaryBgColor,
+    fontSize: 20,
+  },
+})
+
